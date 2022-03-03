3d153059302f7661c70748fccf76142231cb758e
EveKristinLee <jylisa0612@naver.com>
2022-03-04 04:09:57 +0900

    boj

--- 9663 N-Queen.cpp
+++ 9663 N-Queen.cpp
@@ -3,39 +3,41 @@
 
 using namespace std;
 
-int N;
-int board[16];
+int n;
+int map[16] = { 0 };
 int cnt = 0;
 
-bool check(int row) {
-	for (int i = 0; i < row; i++) {
-		//같은 라인이거나 대각선에 위치하면
-		if (board[i] == board[row] || abs(board[row] - board[i]) == row - i) {
+//대각선에 존재하는 좌표의 경우
+//(x, y) (a,b)
+//x - a == y - b
+bool check(int level) {
+	for (int i = 0; i < level; i++) {
+		if (map[level] == map[i] || level - i == abs(map[level] - map[i]))
 			return false;
-			//board[i] == x좌표, i == y좌표
-		}
-		return true;
+		//map[i] = x좌표
+		//i = y좌표
 	}
+	return true;
 }
 
-void nqueen(int x) {
-	if (x == N) {
-		cnt++; //N번째 행까지 배치 완료 하면 방법의 개수 증가
+void n_Queen(int x) {
+	if (x == n) {
+		cnt++;
 	}
 	else {
-		for (int i = 0; i < N; i++) {
-			board[x] = i;
-
-			if (check(x)) {//유효하면 다음행에 퀸 배치, 아니면 다른위치로 변경
-				nqueen(x + 1);
+		for (int i = 0; i < n; i++) {
+			map[x] = i;
+			if (check(x)) {
+				n_Queen(x + 1);
 			}
 		}
 	}
 }
 
+
 int main(void) {
-	cin >> N;
-	nqueen(0);
+	cin >> n;
+	n_Queen(0);
 	cout << cnt << "\n";
 	return 0;
 }
