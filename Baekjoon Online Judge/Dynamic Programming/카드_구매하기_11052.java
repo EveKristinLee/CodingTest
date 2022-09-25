7023bf4c1926fb0898f9ba28d5c769e43c16dd37
이지영 <jylisa0612@naver.com>
2022-09-25 19:51:16 +0900

    boj

---
+++ Baekjoon Online Judge/Dynamic Programming/카드_구매하기_11052.java
@@ -0,0 +1,26 @@
+package solving.solve_0925;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 카드_구매하기_11052 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] num = new int[n+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] dp = new int[n+1];
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				dp[i] = Math.max(dp[i], dp[i-j] + num[j]);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
