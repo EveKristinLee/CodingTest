66dc255f668b3655bc83225b15facfaca9503443
EveKritinLee <jylisa0612@naver.com>
2023-01-25 23:26:04 +0900

    boj

---
+++ Baekjoon Online Judge/B5/검증수_2475.java
@@ -0,0 +1,20 @@
+package solve_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//BOJ B5
+public class 검증수_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<5; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            sum += (n * n);
+        }
+        System.out.println(sum%10);
+    }
+}
