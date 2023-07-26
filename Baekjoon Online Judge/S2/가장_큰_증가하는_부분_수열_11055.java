e17d0379ca2e08c39fc34bbb1b68252ad80412e4
EveKristinLee <jylisa0612@naver.com>
2023-07-27 00:18:13 +0900

    boj

---
+++ Baekjoon Online Judge/S2/가장_큰_증가하는_부분_수열_11055.java
@@ -0,0 +1,36 @@
+//BOJ S2
+package algo_0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장_큰_증가하는_부분_수열_11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = nums[0];
+        for(int i=1; i<N; i++) {
+            dp[i] = nums[i];
+            for(int j=0; j<i; j++) {
+                if(nums[j] < nums[i] && dp[j] + nums[i] >= dp[i]) {
+                    dp[i] = dp[j] + nums[i];
+                }
+            }
+        }
+        int res = 0;
+        for(int i=0; i<N; i++) {
+            res = Math.max(dp[i], res);
+        }
+        System.out.println(res);
+    }
+}
