b92e44c5c20defe6d98835adf2f38c28b27070f7
EveKristinLee <jylisa0612@naver.com>
2023-08-10 01:52:26 +0900

    boj

--- Baekjoon Online Judge/G3/레이저_통신_6087.java
+++ Baekjoon Online Judge/G3/레이저_통신_6087.java
@@ -7,15 +7,31 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 레이저_통신_6087 {
+    static class L implements Comparable<L>{
+        int x;
+        int y;
+        int cnt;
+        int dir;
+
+        public L(int x, int y, int cnt, int dir) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+            this.dir = dir;
+        }
+
+        @Override
+        public int compareTo(L o) {
+            return this.cnt - o.cnt;
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int W; //가로
     static int H; //세로
     static char[][] map;
-    static int sx;
-    static int sy;
-    static int ex;
-    static int ey;
-    static int res;
-    static int[][] visit;
+    static int sx, sy;
+    static int INF = 100 * 100 + 1;
 
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1}; //상, 하, 좌, 우
@@ -28,79 +44,62 @@ public class 레이저_통신_6087 {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         W = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
         map = new char[H][W];
-        visit = new int[H][W];
-        for(int i=0; i<H; i++) {
-            Arrays.fill(visit[i], Integer.MAX_VALUE);
-        }
         boolean existStart = false;
         for(int i=0; i<H; i++) {
             String s = br.readLine();
-            map[i] = s.toCharArray();
             for(int j=0; j<W; j++) {
+                map[i][j] = s.charAt(j);
                 if(!existStart && map[i][j] == 'C'){
                     sx = i;
                     sy = j;
+                    map[i][j] = 'S';
                     existStart = true;
-                }
-                else if(existStart && map[i][j] == 'C') {
-                    ex = i;
-                    ey = j;
                 }
             }
         }
-        res = Integer.MAX_VALUE;
-        bfs(sx, sy);
-        System.out.println(res-1);
+        bfs();
     }
 
-    private static void bfs(int x, int y) {
-        PriorityQueue<Integer[]> q = new PriorityQueue<>((o1, o2) -> {
-            int cnt1 = o1[2];
-            int cnt2 = o2[2];
-
-            return cnt1 - cnt2;
-        });
-
-        q.offer(new Integer[] {x, y, 0, -1});
-
-        visit[x][y] = 0;
+    private static void bfs() {
+        Queue<L> q = new PriorityQueue<>();
+        //visit배열에 x,y지점에서 거울의 최소 개수를 저장하는데,
+        //2차원 배열일 경우에는 해당 자리에 들어올 때 어느 방향에서 들어오는지 모르기 때문에 같거나 크면 q에 다시 넣어줘야헸다
+        //하지만 3차원 배열을 통해서 어느 방향에서 들어올 때 최소 방향인지를 잡아주면
+        //같은 경우를 배제할 수 있기 때문에 q에 넣어주는 작업을 하지 않아도 된다.
+        int[][][] visit = new int[4][H][W];
+        for(int d=0; d<4; d++) {
+            for(int i=0; i<H; i++) {
+                Arrays.fill(visit[d][i], INF);
+            }
+        }
+        q.offer(new L(sx, sy, 0, -1));
 
         while (!q.isEmpty()) {
-            Integer[] top = q.poll();
-            x = top[0];
-            y = top[1];
-            int cnt = top[2];
-            int dir = top[3];
+            L top = q.poll();
 
-            if(x == ex && y == ey) {
-                res = cnt;
+            if(map[top.x][top.y] == 'C') {
+                System.out.println(top.cnt-1);
                 return;
             }
 
             for(int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                int nc = cnt;
-                int nd = i;
+                int nx = top.x + dx[i];
+                int ny = top.y + dy[i];
                 if(!isInside(nx, ny) || map[nx][ny] == '*') {
                     continue;
                 }
-                if(nd != -1 && nd != dir) { //시작점이 아니고, 방향이 꺾이면
-                    if(visit[nx][ny] >= nc + 1) {
-                        visit[nx][ny] = nc + 1;
-                        q.offer(new Integer[] {nx, ny, nc + 1, nd});
-                    }
+                L next = new L(nx, ny, top.cnt, i);
+                if(i != top.dir) {
+                    next.cnt++;
                 }
-                else { //방향이 같으면
-                    if(visit[nx][ny] >= nc) {
-                        visit[nx][ny] = nc;
-                        q.offer(new Integer[] {nx, ny, nc, nd});
-                    }
+
+                if(visit[i][nx][ny] > next.cnt) {
+                    visit[i][nx][ny] = next.cnt;
+                    q.offer(next);
                 }
             }
         }
