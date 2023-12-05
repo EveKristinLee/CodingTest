5482bed93c7992b0cf4f3e760c999f8e543047a7
EveKristinLee <jylisa0612@naver.com>
2023-12-05 19:52:50 +0900

    boj

--- Baekjoon Online Judge/G4/BOJ_2133.java
+++ Baekjoon Online Judge/G4/BOJ_2133.java
@@ -9,11 +9,11 @@ public class BOJ_2133 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[31];
-        dp[0] = 1;
-        dp[1] = 0;
-        dp[2] = 3;
-        if(N > 3 && N % 2 == 0) {
+        int[] dp = new int[N+1];
+        if(N % 2 == 0) {
+            dp[0] = 1;
+            dp[1] = 0;
+            dp[2] = 3;
             for(int i=4; i<=N; i++) {
                 dp[i] = dp[i-2] * dp[2];
                 for(int j=i-4; j>=0; j-=2) {
