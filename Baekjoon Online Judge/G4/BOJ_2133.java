b7561f2f4f657c1c6cfbfcea4671d07bbedb3eb2
EveKristinLee <jylisa0612@naver.com>
2023-12-05 15:35:50 +0900

    boj

---
+++ Baekjoon Online Judge/G4/BOJ_2133.java
@@ -0,0 +1,26 @@
+package solving.solve_1028;
+//BOJ G4 2133 타일 채우기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2133 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[31];
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 3;
+        if(N > 3 && N % 2 == 0) {
+            for(int i=4; i<=N; i++) {
+                dp[i] = dp[i-2] * dp[2];
+                for(int j=i-4; j>=0; j-=2) {
+                    dp[i] += dp[j] * 2;
+                }
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
